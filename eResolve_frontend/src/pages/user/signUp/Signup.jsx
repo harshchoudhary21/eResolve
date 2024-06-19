e59624c4e1fcb02579a9f13f542a0c903c2f051a
Pallav46 <pallavkumar6200@gmail.com>
2024-06-19 11:10:04 +0530

    Initial frontend

---
+++ eResolve_frontend/src/pages/user/signUp/Signup.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+const Signup = () => {
+  return (
+    <div>
+      <h2>Signup Page</h2>
+      {/* Add your signup page content here */}
+    </div>
+  );
+}
+
+export default Signup;
